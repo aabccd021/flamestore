1dd0e777e7c1885cfff2c1a6d1e5a5a26511a5ad
aabccd021 <aabccd021@gmail.com>
2020-10-24 07:00:39 +0700

    changed directory

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import generateRule from './code-generator/rule-generator';
 const rawSchema = getSchema('flamestore.json');
 validate(rawSchema);
 const schema: FlamestoreSchema = rawSchema;
-const rulePath = schema.configuration.ruleOutputPath || "firebase/functions/firestore.rules";
+const rulePath = schema.configuration.ruleOutputPath || "firebase/firestore/firestore.rules";
 generateRule(schema, rulePath);
 const triggerPath = schema.configuration.triggerOutputPath || "firebase/functions/src/flamestore/";
 generateTrigger(schema, triggerPath);
